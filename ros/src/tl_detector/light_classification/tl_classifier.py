a943154b694acb03cfe7b23b697222e89bd13feb
Markus Hollstein <mhollstein1@gmx.de>
2019-02-04 05:45:17 -0800

    first step towards light classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,37 @@
 from styx_msgs.msg import TrafficLight
 
+import numpy as np
+import tensorflow as tf
+
+import rospy
+import os
+import sys
+import cv2
+from glob import glob
+from PIL import Image
+#import numpy as np
+#import tensorflow as tf
+#import matplotlib.pyplot as plt
+#from collections import defaultdict
+#rom utils import label_map_util
+#from utils import visualization_utils as vis_util
+#import randomimport os
+#import sys
+#from glob import glob
+#from PIL import Image
+
+
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        ##TODO load classifier
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(r'./frozen_inference_graph.pb', 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        #pass
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +44,36 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        np_image = self.load_image_into_numpy_array(image)
+        image_expanded = np.expand_dims(np_image, axis=0)
+        with self.graph.as_default():
+            with tf.Session(graph=self.graph) as sess:
+                image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+                detect_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+                detect_scores = self.graph.get_tensor_by_name('detection_scores:0')
+                detect_classes = self.graph.get_tensor_by_name('detection_classes:0')
+                num_detections = self.graph.get_tensor_by_name('num_detections:0')
+                (boxes, scores, classes, num) = sess.run(
+                [detect_boxes, detect_scores, detect_classes, num_detections],
+                feed_dict={image_tensor: image_expanded})
+                rospy.logerr(classes)
+                
+        return self.transform_idx(classes[0][0])
+    	#pass
+    
+    def transform_idx(self,idx):
+        if idx==1:
+            return TrafficLight.GREEN
+        if idx==2: 
+            return TrafficLight.RED
+        if idx ==3:
+            return TrafficLight.YELLOW
+        if idx ==4:
+	    return TrafficLight.UNKNOWN
+	return TrafficLight.UNKNOWN
+
+    def load_image_into_numpy_array(self,image):
+ 
+        np_arr = np.asarray(image[:,:])
+        return np_arr
+
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -44,6 +44,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
+	
         self.light_classifier = TLClassifier()
         self.listener = tf.TransformListener()
 
@@ -119,15 +120,19 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
- #       if(not self.has_image):
- #           self.prev_light_loc = None
- #           return False
+	#if not self.light_classifier:
+	#    return TrafficLight.UNKNOWN
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-#        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        #return self.light_classifier.get_classification(cv_image)
-	return light.state
+        test=  self.light_classifier.get_classification(cv_image)
+	rospy.logerr(test)
+	return test
+	#return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
